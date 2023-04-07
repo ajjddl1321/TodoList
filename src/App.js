d0ffab6b377716e134c998fabdedd0c5a4265775
ajjddl1321 <tjrgh1321@naver.com>
2023-04-07 17:32:38 +0900

    다크모드 추가

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import { useState } from 'react';
 import './App.css';
 import Header from './Header/Header';
 import AppTodo from './component/todoList/TodoList/AppTodo';
+import { DarkModeProvider } from './component/Context/DarkModeContext';
 
 const filters = ['all', 'completed', 'active'];
 
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <>
+    <DarkModeProvider>
       <Header filters = {filters} filter = {filter} onFilterChange = {setFilter}/>
       <AppTodo filter = {filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
